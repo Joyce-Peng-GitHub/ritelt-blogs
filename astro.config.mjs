8730d39922f996bc72424b923b7b4e2fd3b8f889
Joyce <1071060893@qq.com>
2026-04-12 22:47:56 +0800

    Add alert block support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import remarkDirective from 'remark-directive';
+import { remarkAlert } from 'remark-github-blockquote-alert';
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [remarkMath, remarkDirective],
+    remarkPlugins: [remarkMath, remarkDirective, remarkAlert],
     rehypePlugins: [rehypeKatex],
     shikiConfig: {
       themes: {
